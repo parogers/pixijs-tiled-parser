1944ff2da38898cdcaf3fd2f3f42caabc7b0f46a
Peter Rogers <peter.rogers@gmail.com>
2026-08-15 15:12:56 -0400

    fix: use pixi asset loader

--- src/index.ts
+++ src/index.ts
@@ -13,33 +13,48 @@ export { loadTiledMap, TiledMap }
 
 
 const TILED_MAP_LOADER = {
-    id: 'pixijs-tiled-parser/map-loader',
-    extension: {
-        type: PIXI.ExtensionType.LoadParser,
-        name: 'pixijs-tiled-parser-map-loader',
-    },
-    getCacheableAssets(keys: string[], asset: TiledMap) {
-        const result: any = {};
-        keys.forEach(key => {
-            result[key] = asset;
-        })
-        asset.tilesetRefs.forEach(tilesetRef => {
-            const spritesheet = tilesetRef.tileset?.spritesheet;
-            if (!spritesheet) {
-                return;
-            }
-            Object.entries(spritesheet.textures).forEach(([key, value]) => {
-                result[key] = value;
+    extension: PIXI.ExtensionType.Asset,
+    cache: {
+        test(data: any): boolean {
+            return (
+                data &&
+                typeof data === 'object' &&
+                data['rows'] !== undefined &&
+                data['cols'] !== undefined
+            );
+        },
+        getCacheableAssets(keys: string[], asset: TiledMap) {
+            const result: any = {};
+            keys.forEach(key => {
+                result[key] = asset;
+            })
+            asset.tilesetRefs.forEach(tilesetRef => {
+                const spritesheet = tilesetRef.tileset?.spritesheet;
+                if (!spritesheet) {
+                    return;
+                }
+                Object.entries(spritesheet.textures).forEach(([key, value]) => {
+                    result[key] = value;
+                });
             });
-        });
-        return result;
+            return result;
+        },
     },
-    test(url: string) {
-        return url.endsWith('.tmx');
-    },
-    async load(url: string) {
-        const map = await loadTiledMap(url)
-        return map;
+    loader: {
+        id: 'tiledmap',
+        extension: {
+            type: PIXI.ExtensionType.LoadParser,
+            priority: PIXI.LoaderParserPriority.Normal,
+            name: 'tiledMapLoader',
+        },
+        test(url: string) {
+            return url.endsWith('.tmx');
+        },
+        async load(url: string) {
+            const map = await loadTiledMap(url)
+            return map;
+        },
+
     },
 };
 
--- tests/extension.test.ts
+++ tests/extension.test.ts
@@ -4,9 +4,21 @@ import { loadTiledMap } from '../src/index';
 import { makeTiledParserExtension } from '../src/index';
 
 
+test('tests for a map file', async () => {
+    expect(makeTiledParserExtension().loader.test('map.tmx')).toBe(true);
+    expect(makeTiledParserExtension().loader.test('somethingelse.json')).toBe(false);
+});
+
+
+test('loads a map object', async () => {
+    const map = await makeTiledParserExtension().loader.load('tests/empty.tmx');
+    expect(!!map).toBeTruthy();
+});
+
+
 test('caching the map object', async () => {
     const map = await loadTiledMap('tests/empty.tmx');
-    const results = makeTiledParserExtension().getCacheableAssets(['a', 'b', 'c'], map);
+    const results = makeTiledParserExtension().cache.getCacheableAssets(['a', 'b', 'c'], map);
     expect(results['a']).toEqual(map);
     expect(results['b']).toEqual(map);
     expect(results['c']).toEqual(map);
@@ -15,7 +27,7 @@ test('caching the map object', async () => {
 
 test('caching the spritesheets', async () => {
     const map = await loadTiledMap('tests/grid-layers.tmx');
-    const results = makeTiledParserExtension().getCacheableAssets(['a', 'b', 'c'], map);
+    const results = makeTiledParserExtension().cache.getCacheableAssets(['a', 'b', 'c'], map);
     expect(results['tests/tiles-1']).toBeTruthy();
     expect(results['tests/tiles-2']).toBeTruthy();
     expect(results['tests/tiles-9']).toBeTruthy();
